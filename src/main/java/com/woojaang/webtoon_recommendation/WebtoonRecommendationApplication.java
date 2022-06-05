d5233a03f198a3f0ef2d5b36854f7594fa4fef18
woojaang <woojaang@naver.com>
2022-06-05 21:27:33 +0900

    first

---
+++ src/main/java/com/woojaang/webtoon_recommendation/WebtoonRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.woojaang.webtoon_recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtoonRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtoonRecommendationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woojaang/webtoon_recommendation/WebtoonRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woojaang.webtoon_recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtoonRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
